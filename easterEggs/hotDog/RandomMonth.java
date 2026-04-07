3d9750dfa67cbcedeea965d1fd20c58499598aeb
Edmond-1999 <agburugaedmond@gmail.com>
2026-04-07 06:41:29 +0100

    eastereggs

--- easterEggs/hotDog/RandomMonth.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class RandomMonth {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        int month = (int)(Math.random() * 12) + 1;
-        System.out.println(month);
-    }
-}
